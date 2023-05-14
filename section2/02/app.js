3d58c6c69f7d019f6328fa0e540f2526631a9ddf
nukesaku <nextniconico@gmail.com>
2023-05-14 21:04:13 +0900

    section2_2

---
+++ section2/02/app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 10,
+      name: '',
+      confirmedName: '',
+    };
+  },
+  methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert('Submitted!');
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    }
+  }
+});
+
+app.mount('#events');
